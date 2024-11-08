ff510b649ba5fe379d859b10ccb4e0e7d7394f88
Maks Rechnio <maks.rechnio@gmail.com>
2024-11-08 18:10:09 +0100

    build the pages, debugged, all works, ready for designing

--- website/views.py
+++ website/views.py
@@ -5,3 +5,15 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("index.html")
+
+@views.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@views.route('/philosophy')
+def philosophy():
+    return render_template('philosophy.html')
+
+@views.route('/the-offer')
+def the_offer():
+    return render_template('the-offer.html')

07fee1ef780d210dab47af32cbf472dd06d6cb83
Maks Rechnio <maks.rechnio@gmail.com>
2024-11-08 12:25:11 +0100

    created a base with the example navbar, used Jinja to put it into index.html

---
+++ website/static/script.js
@@ -0,0 +1,5 @@
+// Toggles the mobile menu when the hamburger menu is clicked
+function toggleMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('active');
+}

3e45822a1351af8f501b11bcc6f657370595d2a8
Maks Rechnio <maks.rechnio@gmail.com>
2024-12-16 23:38:31 +0100

    finished the design of the contact page - with inquiry form - not working, desktop design

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
@@ -17,3 +17,17 @@ def philosophy():
 @views.route('/the-offer')
 def the_offer():
     return render_template('the-offer.html')
+
+#This is the function to send the inquiry on the contact page
+@views.route('/submit_inquiry', methods=['POST'])
+def submit_inquiry():
+    name = request.form.get('name')
+    surname = request.form.get('surname')
+    company = request.form.get('company')
+    email = request.form.get('email')
+    project = request.form.get('project')
+
+    # Process the form data as needed
+    print(f"Name: {name}, Surname: {surname}, Company: {company}, Email: {email}, Project: {project}")
+
+    return f"Thank you for your inquiry, {name}! We'll get back to you shortly."
